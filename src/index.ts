6ecc896a60cbbdf2fbe5643cac5969c93a1f313f
Stéphane <stephane.pires.learning@gmail.com>
2021-08-01 15:52:13 +0200

    chore(tmp): 🚧 fichiers temporaires pour tester trivago/sort-import pour prettier

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+// Test import one
+import { CONST } from './test-import-one/test-import'
+
+// Test import two
+import { TEST } from './test-import-two/test-import-2'
+
 console.log('Hello World!')
 
 const a = 3
@@ -9,3 +15,6 @@ function multiplyByTwo(number: number) {
 
 multiplyByTwo(a)
 multiplyByTwo(b)
+
+console.log('CONST', CONST)
+console.log('TEST', TEST)
