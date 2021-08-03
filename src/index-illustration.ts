b3ca11f80d623ad2d8501924186e5f576675edd7
Stéphane Pires <stephane.pires@analogway.com>
2021-08-03 11:20:48 +0200

    feat: nouvel exemple d'illustration (pour les messages de commit linter)

---
+++ src/index-illustration.ts
@@ -0,0 +1,10 @@
+// Constants
+import { three } from './constants/fake-constant-a'
+import { four } from './constants/fake-constant-b'
+
+// Utils
+import { multiplyByThree } from './utils/fake-utils-by-three'
+import { multiplyByTwo } from './utils/fake-utils-by-two'
+
+multiplyByThree(four)
+multiplyByTwo(three)
